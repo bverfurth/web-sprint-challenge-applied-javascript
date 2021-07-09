fa2b3bbc20e8c04e0a249c4772bfd46f0e091133
bverfurth <78745926+bverfurth@users.noreply.github.com>
2021-07-09 16:21:19 -0400

    task 6 works. i messed up the get url in both spots. dumb mistake

--- src/components/card.js
+++ src/components/card.js
@@ -63,7 +63,7 @@ const Card = (article) => {
 const cardAppender = (selector) => {
   const array = ["javascript", "bootstrap", "technology", "jquery", "node"];
   axios
-    .get(`https://lambda-times-api.herokuapp.com/articles`)
+    .get(`http://localhost:5000/api/articles`)
     .then((res) => {
       array.forEach((topic) => {
         res.data.articles[topic].forEach((el) => {
